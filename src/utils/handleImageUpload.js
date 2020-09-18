9422250046ed1eb08ddb09cad691451374acbefd
kaung khant thar <kaungkhantthar@ucsm.edu.mm>
2020-09-18 08:17:58 +0630

    Change upload setting

--- src/utils/handleImageUpload.js
+++ src/utils/handleImageUpload.js
@@ -1,16 +1,13 @@
-async function handleImageUpload(image, uploadPreset = 'instagram') {
+async function handleImageUpload(image, uploadPreset = "instagram") {
   const data = new FormData();
   data.append("file", image);
-  data.append("upload_preset", "instagram");
+  data.append("upload_preset", uploadPreset);
   data.append("cloud_name", "dpifeeqlv");
-  const res = await fetch(
-    "https://api.cloudinary.com/v1_1/dpifeeqlv/image/upload",
-    {
-      method: "POST",
-      accept: "application/json",
-      body: data,
-    }
-  );
+  const res = await fetch("https://api.cloudinary.com/v1_1/dpifeeqlv/image/upload", {
+    method: "POST",
+    accept: "application/json",
+    body: data,
+  });
   const jsonRes = await res.json();
   console.log(jsonRes);
   return jsonRes.secure_url;
